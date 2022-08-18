2a829e85ff80d7a9c047089ec068e926ca26d222
akash.yanpure <akash.yanpure@gmail.com>
2022-08-18 21:04:35 +0100

    csv download complete

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response, send_file
 import psycopg2
 import os
 import csv
 import xlwt
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
@@ -53,47 +54,66 @@ def get_user_details():
 
 @app.route("/getPlotCSV", methods=["GET"])
 def getPlotCSV():
-    data = []
-    output_file_1= "test.csv"
     try:
         conn = psycopg2.connect(dbname="postgres",
                                 host='test-db-postgres.c8cxqgsirbs0.eu-west-2.rds.amazonaws.com',
                                 user="postgres", password="mystrongpassword")
         cur = conn.cursor()
+        cur.execute('''SELECT * FROM public.user_details order by id;''')
+        rows=cur.fetchall()
+        
+        # Generate CSV FILE
+        workbook = xlwt.Workbook()
+        worksheet = workbook.add_sheet(os.path.split('output.csv')[1])
+
+        worksheet.set_panes_frozen(True)
+        worksheet.set_horz_split_pos(0)
+        worksheet.set_remove_splits(True)
+
+        for colidx,heading in enumerate(cur.description):
+            worksheet.write(0,colidx,heading[0]) # first element of each tuple
+
+        # Write rows
+        for rowidx, row in enumerate(rows):
+            for colindex, col in enumerate(row):
+                worksheet.write(rowidx+1, colindex, col)
+
+        # All done
+        workbook.save('output.csv')
+
+        csv_file = "output.csv"
+        csv_path = os.path.join(csv_file)
+        
+        # Also make sure the requested csv file does exist
+        if not os.path.isfile(csv_path):
+            return "ERROR: file %s was not found on the server" % csv_file
+        # Send the file back to the client
+        conn.close()
+        return send_file(csv_path, as_attachment=True,  mimetype='text/csv', download_name=csv_file)
+
     except Exception as e:
         return(str(e))
 
-    try:
-        cur.execute('''SELECT * FROM public.user_details order by id ;''')
-    except:
-        print("Enable to execute query")
 
-    rows=cur.fetchall()
 
-    workbook = xlwt.Workbook()
-    worksheet = workbook.add_sheet(os.path.split(output_file_1)[1])
+    # workbook = xlwt.Workbook()
+    # worksheet = workbook.add_sheet(os.path.split('output.csv')[1])
 
-    worksheet.set_panes_frozen(True)
-    worksheet.set_horz_split_pos(0)
-    worksheet.set_remove_splits(True)
+    # worksheet.set_panes_frozen(True)
+    # worksheet.set_horz_split_pos(0)
+    # worksheet.set_remove_splits(True)
 
-    for colidx,heading in enumerate(cur.description):
-        worksheet.write(0,colidx,heading[0]) # first element of each tuple
+    # for colidx,heading in enumerate(cur.description):
+    #     worksheet.write(0,colidx,heading[0]) # first element of each tuple
 
-    # Write rows
-    for rowidx, row in enumerate(rows):
-        for colindex, col in enumerate(row):
-            worksheet.write(rowidx+1, colindex, col)
+    # # Write rows
+    # for rowidx, row in enumerate(rows):
+    #     for colindex, col in enumerate(row):
+    #         worksheet.write(rowidx+1, colindex, col)
 
     # All done
-    workbook.save(output_file_1)
-
-    print("Download process done!!")
-    conn.commit()
-    conn.close()
-
-    return "File downloaded successfully!"
+    # workbook.save('output.csv')
 
 if __name__ == "__main__":
     port = 5000
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
